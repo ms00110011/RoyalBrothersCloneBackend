1b9021f6f7431b338b2d361757c1ac975c1516fb
ms00110011 <ms00110011@gmail.com>
2022-05-09 15:47:49 +0530

    .

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const http = require("http");
 const app = require("./Routes/app");
 const connectToDB = require("./DBConnection/MongoDB");
 
-const port = 9008;
+const PORT = process.env.PORT || 9008;
 
-http.createServer(app).listen(port, () => {
+http.createServer(app).listen(PORT, () => {
   new connectToDB();
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${PORT}`);
 });
